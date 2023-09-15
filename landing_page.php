c04a5be6f06b4f1467ecbbce460832d58a25fd7d
Dyego Magno <dyegowolf@gmail.com>
2023-09-15 06:30:39 -0300

    First commit

---
+++ landing_page.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Fale Conosco - FAESA</title>
+
+        <!-- Import do CSS !-->
+        <link rel="stylesheet" type="text/css" href="stylesheet.css" />
+
+    </head>
+
+    <body>
+        <form action="" method="POST">
+            <p>
+                Contato
+            </p>
+
+            <div class="input">
+                <input name="name" type="text" placeholder="Por favor, insira seu nome"/>
+                <input name="email" type="email" placeholder="Por favor, insira seu email"/>
+                <input name="phone" type="tel" placeholder="Por favor, insira seu telefone"/>
+                <input name="message" placeholder="Sua mensagem aqui"/>
+            </div>
+        </form>
+    </body>
+</html>
